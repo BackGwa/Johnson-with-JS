4937cfb8c81793586293e60d5b0c824eee7d8694
BackGwa <64456214+BackGwa@users.noreply.github.com>
2023-01-04 19:47:55 +0900

    CreateMealText 함수 추가

--- script/JohnsonLib.js
+++ script/JohnsonLib.js
@@ -119,3 +119,24 @@ async function MealRequest(SchoolType, SchoolCode, Date = NowDate(), Allergy = f
     return Result;
   }
 };
+
+
+/**
+ * ---
+ * ## `CreateMealText`
+ * ### 파라미터에 입력 된 값을 기반으로 API에 요청하고 그 정보를 파싱하여 설정 된 `div`에 포함합니다.
+ * - 다음 `CreateMealText` 함수는 반환 할 급식 정보에 대한 추가 파라미터를 지정할 수 없습니다.
+ * ---
+ * @param {String} divName 급식 정보를 포함할 `div`의 `id`
+ * @param {String} SchoolType 급식 정보를 반환할 학교의 유형
+ * @param {String} SchoolCode 급식 정보를 반환할 학교의 고유코드
+ * ---
+ * @returns 파라미터에 입력 된 값을 기반으로 API에 요청하고 그 정보를 파싱하여 설정 된 `div`에 포함합니다.
+ * ---
+*/
+
+async function CreateMealText(divName, SchoolType, SchoolCode){
+  const meal = await MealRequest(SchoolType, SchoolCode);
+      const mealElement = document.getElementById(divName);
+      mealElement.innerHTML = meal.join("<br>");
+}
